585b7a8bfad0128274349a44dcbe454325790794
Tim Roche <roche.tp@gmail.com>
2014-09-18 12:37:53 -0700

    mid day commit

--- sandbox/lua/user_scripts/temperature_exceeds_value.lua
+++ sandbox/lua/user_scripts/temperature_exceeds_value.lua
@@ -5,12 +5,12 @@ Example of a user script
 return function()
     local utils = require "lua/system/utils_3scale"
     local cjson = require "cjson"
-    local zlib = require "lzlib"
+    local zlib = require "zlib"
   local path = utils.split(ngx.var.request," ")[2]
   local res = ngx.location.capture("/api/da76745e63b41051/geolookup/q/94110.json")
   --ngx.header.content_type = "text/html"
   --ngx.say(path.." rules!")
-    local result = cjson.decode(res.body)
-    ngx.print(result)
+    --local result = cjson.decode(res.body)
+    ngx.say(res.body)
   ngx.exit(ngx.HTTP_OK)
 end
