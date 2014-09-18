f7ee9ef2fc1c221d31dc81da74014ede0fa10992
Tim Roche <roche.tp@gmail.com>
2014-09-18 15:33:22 -0700

    working example

--- sandbox/lua/user_scripts/temperature_exceeds_value.lua
+++ sandbox/lua/user_scripts/temperature_exceeds_value.lua
@@ -6,11 +6,18 @@ return function()
     local utils = require "lua/system/utils_3scale"
     local cjson = require "cjson"
     local zlib = require "zlib"
-  local path = utils.split(ngx.var.request," ")[2]
-  local res = ngx.location.capture("/api/da76745e63b41051/geolookup/q/94110.json")
+    local path = utils.split(ngx.var.request," ")[2]
+    local res = ngx.location.capture("/api/da76745e63b41051/geolookup/q/94110.json")
+    local buff = res.body
+   -- local response=zlib.inflate(buff)
+    local stream_in = zlib.inflate(buff)
+    local result = stream_in:read("*a")
+     stream_in:close()
+    --local response=zlib.version()
+
   --ngx.header.content_type = "text/html"
   --ngx.say(path.." rules!")
     --local result = cjson.decode(res.body)
-    ngx.say(res.body)
+    ngx.say(result)
   ngx.exit(ngx.HTTP_OK)
 end
