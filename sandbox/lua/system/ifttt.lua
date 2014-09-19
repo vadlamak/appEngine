f1c9427542ff9dc22dcb3facabadbd31a72297ca
Tim Roche <roche.tp@gmail.com>
2014-09-18 17:51:10 -0700

    added productinfo script

--- sandbox/lua/system/ifttt.lua
+++ sandbox/lua/system/ifttt.lua
@@ -1,64 +1,6 @@
---[[
-
-Run the API aggregation scripts on a sandboxed environment
-
-In the table sandboxed_env you define all the functions that the api aggregation
-lua scripts will be able to use. For instance, we can limit the functions available
-on the nginx module: { var = ngx.var, re = ngx.re, location = ngx.location, 
-header = ngx.header, exit = ngx.exit, say = ngx.say, HTTP_OK = ngx.HTTP_OK}
-
-You can be as restrictive as you want. Note that each request in nginx runs the lua
-script in a different lua virtual machine, so it's not possible to cross-contaminate 
-requests.
-
-Check http://lua-users.org/wiki/SandBoxes for more pointers
-
-Additionally, you can set up a timeout of the script by setting a value on,
-
-debug.sethook(timeout_response, "", 5000)
-
-The 5000 is the number of operations that the lua script can execute before timing
-out. This will kill automatically any long running requests. Tuning this parameters
-depends on the complexity of the scripts that you allow to run. 5000 is plenty for 
-the sentence_with_highest_word.lua 
-
-]]--
-
-sandboxed_env = {
-  ipairs = ipairs,
-  next = next,
-  pairs = pairs,
-  pcall = pcall,
-  tonumber = tonumber,
-  tostring = tostring,
-  type = type,
-  unpack = unpack,
-  string = { byte = string.byte, char = string.char, find = string.find,
-      format = string.format, gmatch = string.gmatch, gsub = string.gsub,
-      len = string.len, lower = string.lower, match = string.match,
-      rep = string.rep, reverse = string.reverse, sub = string.sub,
-      upper = string.upper },
-  table = { insert = table.insert, maxn = table.maxn, remove = table.remove,
-      sort = table.sort },
-  math = { abs = math.abs, acos = math.acos, asin = math.asin,
-      atan = math.atan, atan2 = math.atan2, ceil = math.ceil, cos = math.cos,
-      cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor,
-      fmod = math.fmod, frexp = math.frexp, huge = math.huge,
-      ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max,
-      min = math.min, modf = math.modf, pi = math.pi, pow = math.pow,
-      rad = math.rad, random = math.random, sin = math.sin, sinh = math.sinh,
-      sqrt = math.sqrt, tan = math.tan, tanh = math.tanh },
-  os = { clock = os.clock, difftime = os.difftime, time = os.time },
-  print = print,
-  utils = require "lua/system/utils_3scale",
-  ngx = ngx
-}
-
---[[
-  Needs to file which file to lua based on the URL
-]]--
 
 local utils = require "lua/system/utils_3scale"
+local getProductInfo = require "lua/system/getProductInfo"
 --local cjson = require "cjson"
 local path = utils.split(ngx.var.request," ")[2]
 --local user_script_file = ngx.re.match(path,[=[^\/v1\/triggers\/([a-zA-Z0-9-_]+)]=])[1]
@@ -66,20 +8,17 @@ local theSplit = ngx.re.match(path,[=[^\/v1\/geocode\/([-+]?[0-9]*\.?[0-9]+)\/([
 local lat  = theSplit[1]
 local lon = theSplit[2]
 local user_script_file = theSplit[3]
---[[
-ngx.say("path")
-ngx.say(path)
-ngx.say(lat)
-ngx.say(lon)
-ngx.say("user_script_file")
-ngx.say(user_script_file)
-]]--
+
+local atomicApi = getProductInfo.getAtomicApi(user_script_file)
+    --ngx.say(type(getProductInfo))
+    ngx.say(atomicApi)
 
 lc = loadfile(ngx.var.lua_user_scripts_path..user_script_file..".lua")
-ngx.say(ngx.var.lua_user_scripts_path..user_script_file..".lua")
-ngx.say("hello?")
---dofile(ngx.var.lua_user_scripts_path..user_script_file..".lua")
---lc=nil
+--ngx.say(ngx.var.lua_user_scripts_path..user_script_file..".lua")
+--ngx.say("hello?")
+
+local args = ngx.req.get_uri_args()
+--for k, v in pairs(args) do ngx.say(k,v) end
 
 if (lc == nil) then
   ngx.exit(ngx.HTTP_NOT_FOUND)
@@ -94,16 +33,4 @@ local timeout_response = function()
   debug.sethook()
   error("The lua script tem_exceeds has timed out!!")
 end
---pcall(user_function)
 user_function();
-ngx.say("called user func")
-
---[[
-debug.sethook(timeout_response, "", 5000)
-setfenv(user_function, sandboxed_env)
-local res_user_function = pcall(user_function)
-if not res_user_function then
-  error("There was an error running temp_exceeds")
-end
-debug.sethook()
-]]--
--- sandbox/lua/user_scripts/temperature_exceeds_value.lua
+++ sandbox/lua/user_scripts/temperature_exceeds_value.lua
@@ -6,24 +6,12 @@ return function()
     local utils = require "lua/system/utils_3scale"
     local cjson = require "cjson"
     local zlib = require "zlib"
-    local path = utils.split(ngx.var.request," ")[2]
-    local res = ngx.location.capture("/api/da76745e63b41051/geolookup/q/94110.json")
-    local buff = res.body
-   -- local response=zlib.inflate(buff)
-    local stream_in = zlib.inflate(buff)
-    ngx.say(getmetatable(stream_in))
-    local content
-    if (stream_in==nil) then
-        content=res.body
-    else
-        content = stream_in:read("*a")
-        stream_in:close()
-    end
-    --local response=zlib.version()
 
-  --ngx.header.content_type = "text/html"
-  --ngx.say(path.." rules!")
-    --local result = cjson.decode(res.body)
+    local res = ngx.location.capture("/api/da76745e63b41051/geolookup/q/94110.json")
+    local stream_in = zlib.inflate(res.body)
+    local content = stream_in:read("*a")
+    stream_in:close()
+
     ngx.say(content)
-  ngx.exit(ngx.HTTP_OK)
+    ngx.exit(ngx.HTTP_OK)
 end
